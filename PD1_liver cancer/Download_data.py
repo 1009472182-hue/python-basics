33553a1e2c0cc9bbf8b1d02b7deefddeb25f5e54
1009472182-hue <1009472182@qq.com>
2025-12-10 16:19:09 +0800

    读取FAERS数据库

--- PD1_liver cancer/Download_data.py
+++ PD1_liver cancer/Download_data.py
@@ -4,11 +4,17 @@ import requests
 # 下载 FAERS ASCII 的脚本
 
 # 设置保存目录
-out_dir = "faers_data"
+out_dir = r"E:\学习资料\硕士\广医二院相关资料\PD-1免疫抑制剂治疗肝癌的临床综合评价\FAERS数据库"
 os.makedirs(out_dir, exist_ok=True)
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+}
+
 # 定义要下载的年份和季度
-years = list(range(2015, 2026))  # 例如 2015–2025 年
+years = list(range(2020, 2021))  # 例如 2014–2020 年
 quarters = ["Q1", "Q2", "Q3", "Q4"]
 
 base_url = "https://fis.fda.gov/content/Exports/faers_ascii_{year}{quarter}.zip"
@@ -19,7 +25,7 @@ for year in years:
         filename = os.path.join(out_dir, f"faers_ascii_{year}{q}.zip")
         try:
             print(f"Downloading {url} ...")
-            resp = requests.get(url, timeout=60)
+            resp = requests.get(url, headers=headers, timeout=60, stream=True)
             if resp.status_code == 200:
                 with open(filename, "wb") as f:
                     f.write(resp.content)
